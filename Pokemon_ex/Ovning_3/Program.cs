e0cc7369887395ae0e1ff358cc9305c197e42310
Albin <albin@bohlin.nu>
2025-05-28 15:38:22 +0200

    Moved Readme

--- Pokemon_ex/Ovning_3/Program.cs
+++ Pokemon_ex/Ovning_3/Program.cs
@@ -8,8 +8,8 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-            var c = Type.Water;
-            var flamethrower = new Attack("Flamethrower", Type.Fire, 12)
+            var c = ElementType.Water;
+            var flamethrower = new Attack("Flamethrower", ElementType.Fire, 12);
 
 
         }
